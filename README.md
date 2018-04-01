d7957dae01ab5acf49835f0ac8bb4147315afcac
jeanwei <jeanwei938@msn.com>
2018-04-01 15:41:18 +0800

    template to use p5.js

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-### Wikipedia Crawler with P5 (from Coding Train)
-
-loadJSON() with callback is used:
- - to get a list of search results
- - to browse to a search result page
-regex is used to filter JSON data returned by wiki API
+### Template using P5.js
 
 
-### Issue faced
+To start the server at port 12000:
 ```
-No 'Access-Control-Allow-Origin' header is present on the requested resource.
-Origin 'http://localhost:12000' is therefore not allowed access.
-If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+$ npm run dev
 ```
-workaround: use 'jsonp' polyfill
--- src/js/sketch.js
+++ src/js/sketch.js
@@ -20,54 +20,9 @@ const sketch = (p5) => {
   //   p5.frameRate(10);
   // }
 
-  let userInput;
-  let searchUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
-  let contentUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=";
-  let counter = 0;
-
   p5.setup = () => {
-    p5.noCanvas();
-    userInput = p5.select('#userinput');
-    userInput.changed(startSearch);
   }
 
-  function startSearch() {
-    counter = 0;
-    goWiki(userInput.value());
-  }
-
-  function goWiki(term) {
-    counter = counter + 1;
-    if (counter < 10) {
-      let url = searchUrl + term;
-      p5.loadJSON(url, gotSearch, 'jsonp');
-    }
-  }
-
-  function gotSearch(data) {
-    let len = data[1].length;
-    let index = p5.floor(p5.random(len));
-    let title = data[1][index];
-    title = title.replace(/\s+/g, '_');
-    p5.createP(title);
-    console.log('Querying: ' + title);
-
-    let url = contentUrl + title;
-    p5.loadJSON(url, gotContent, 'jsonp');
-  }
-
-  function gotContent(data) {
-    let page = data.query.pages
-    let pageId = Object.keys(data.query.pages)[0];
-    console.log(pageId);
-    let content = page[pageId].revisions[0]['*'];
-    console.log(content);
-    let wordRegex = /\b\w{4,}\b/g;
-    var words = content.match(wordRegex);
-    var word = p5.random(words);
-    goWiki(word);
-    console.log(word);
-  }
 
   // Draw function
   // ======================================
